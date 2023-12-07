91eac0a1895a9b4051ce23f8e52e07512d4e24c4
ChenYang Yu <alumi5566@gmail.com>
2023-12-06 21:54:50 -0800

    Add thread leak bucket

--- Atlassian/RL_LeakBucket_Thread.py
+++ Atlassian/RL_LeakBucket_Thread.py
@@ -1,61 +1,42 @@
-from time import time, sleep
-""""
-Picture a bucket in your mind.
-Fill the buckets with tokens at a constant rate.
-When a packet arrives, check if there is any token in the bucket.
-If there was any token left, remove one from the bucket and forward the packet. If the bucket was empty, simply drop the packet.
-"""
-class LeakBucket:
+from threading import Lock, Thread
+from datetime import datetime
+from _collections import deque
+import time
+import sys
 
-    def __init__(self, maxBucket, time_unit, forward_callback, drop_callback):
-        # time_unit: the tokens are added in this time frame.
-        self.time_unit = time_unit
-        self.forward_callback = forward_callback
-        self.drop_callback = drop_callback
-        # maxBucket the size of the bucket
-        self.maxBucket = maxBucket
-        self.stack = []
-        # last_check is the timestamp that we previously handled a packet.
-        self.last_check = int(time())
+lock = Lock()
+queue = deque()
+BUCKET_CAPACITY = 100
+LEAK_PER_SECOND = 1
+REQUEST_PER_SECOND = 10
 
-    def handle(self, packet):
-        # if current time is in the next time frame, reset and allow max capacity to process
-        if int(time()) >= self.last_check + self.time_unit:
-            while len(self.stack) > 0:
-                self.forward_callback(self.stack[0])
-                self.stack.pop(0)
-            self.last_check = int(time())
-            # self.allowance = self.capacity
+def process():
+    if not len(queue):
+        return None
+    with lock:
+        return queue.popleft()
 
-        if (len(self.stack) >= self.maxBucket):
-            return self.drop_callback(packet)
+def request():
+    if len(queue) >= BUCKET_CAPACITY:
+        return False
+    with lock:
+        queue.append(datetime.now())
+        return True
 
-        self.stack.append(packet)
-        return
-        # return self.forward_callback(packet)
+def process_in_seq():
+    while True:
+        time.sleep(1 / LEAK_PER_SECOND)
+        res = process()
+        if res:
+            print(f'{datetime.now(): %H:%M:%S} / request processed original request: {res: %H:%M:%S}')
 
+def request_in_seq():
+    while True:
+        time.sleep(1 / REQUEST_PER_SECOND)
+        request()
 
-def forward(packet):
-    print("Packet Forwarded: " + str(packet))
+process_thread = Thread(target=process_in_seq)
+process_thread.start()
 
-
-def drop(packet):
-    print("Packet Dropped: " + str(packet))
-
-# def __init__(self, init_token, maxBucket, time_unit, forward_callback, drop_callback):
-throttle = LeakBucket(1, 1, forward, drop)
-
-packet = 0
-
-# while True:
-#     sleep(0.2)
-#     throttle.handle(packet)
-#     packet += 1
-
-import threading as th
-## Defining a method
-def sctn():
-    print("SECTION FOR LIFE \n")
-S = th.Timer(5.0, sctn)
-S.start()
-print("Exit Program\n")
+request_thread = Thread(target=request_in_seq)
+request_thread.start()
